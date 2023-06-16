902329565e8e9cf8e58fdc59732947a4bae9ec2b
Timur <xanov.timur@mail.ru>
2023-06-16 15:54:38 +0300

    Решили 3 задачу

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Является ли число чётным
+Console.Write("Посмотрим, является ли число чётным. Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+
+else
+{
+    Console.WriteLine("Число нечетное");
+
+}  
